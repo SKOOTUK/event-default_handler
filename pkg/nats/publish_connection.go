872b42fb10dcbf2048b5fcf4c2a99219445d0736
Nathan <pnathan117@gmail.com>
2021-03-10 15:53:53 +0000

    add publish changes

--- pkg/nats/publish_connection.go
+++ pkg/nats/publish_connection.go
@@ -2,10 +2,12 @@ package nats
 
 import (
 	"fmt"
+
+	"github.com/nats-io/stan.go"
 )
 
 //Publish publishes a request to the NATS queue
-func (c *Connection) Publish(m Message) error {
+func (c *Connection) Publish(m stan.Msg) error {
 	select { //non blocking channel - if there is no error will go to default where we do nothing
 	case err := <-c.err:
 		if err != nil {
@@ -14,7 +16,7 @@ func (c *Connection) Publish(m Message) error {
 	default:
 	}
 
-	if err := c.conn.Publish(c.routingKey, m.Body.Data); err != nil {
+	if err := c.conn.Publish(c.routingKey, m.Data); err != nil {
 		return fmt.Errorf("Error in Publishing: %s", err)
 	}
 	return nil
